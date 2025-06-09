8facd497637fab3a467db7d21f48ded5deb5eba9
Roshan7228 <7930roshankumar@gmail.com>
2025-06-09 11:49:01 +0530

    Back-end and Front-end

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,10 +1,10 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ Add this line
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -12,6 +12,6 @@ export default defineConfig({
     },
   },
   server: {
-    host: true, 
+    host: true,
   },
-})
+});
